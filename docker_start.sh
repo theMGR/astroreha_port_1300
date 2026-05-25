b9a7e58a0c416bceb3d207f6fdf6f35fc8fde533
theMGR <manivannan.g@outlook.in>
2026-05-25 22:22:01 +0530

    😁

--- docker_start.sh
+++ docker_start.sh
@@ -11,7 +11,7 @@ echo "Installing dependencies..."
 npm install
 
 echo "Performing clean start in Docker..."
-docker-compose down
+docker compose down
 echo "Building and Starting AstroReha API in Docker..."
-docker-compose up -d --build --force-recreate
+docker compose up -d --build --force-recreate
 echo "Docker container is starting. Access it at http://localhost:1300"
